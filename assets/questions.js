8127230b17019d1cfde8afcceb46af7e6dfbb718
Frank-Colorado <chawnkiee@gmail.com>
2023-04-24 18:12:08 -0400

    created an array called 'quizQuestions'

---
+++ assets/questions.js
@@ -0,0 +1,37 @@
+const quizQuestions = [
+  {
+    q: "Commonly used data types DO NOT include:",
+    options: ["Strings", "Booleans", "Alerts", "Numbers"],
+    answer: 2,
+  },
+  {
+    q: "Arrays in JavaScript can be used to store ____",
+    options: [
+      "Numbers and Strings",
+      "Other Arrays",
+      "Booleans",
+      "All of the Above",
+    ],
+    answer: 3,
+  },
+  {
+    q: "Which HTML element is used to link the JavaScript file?",
+    options: [
+      "<script></script>",
+      "<javascript></javascript>",
+      "<js></js>",
+      "<scripting></scripting>",
+    ],
+    answer: 0,
+  },
+  {
+    q: "The condition in an if / else statement is enclosed with ____",
+    options: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
+    answer: 2,
+  },
+  {
+    q: "String values must be enclosed within ___ when being assigned to variables.",
+    options: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+    answer: 2,
+  },
+];
