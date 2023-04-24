60635e46f95998e7ab6aca127bf3a740b5f02e0c
Frank-Colorado <chawnkiee@gmail.com>
2023-04-23 21:09:31 -0400

    added variables to script,js file

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// DOM Navbar Variables
+const timer = document.getElementById("timer");
+// DOM DIVs
+const homePageDiv = document.getElementById("home");
+const quizDiv = document.getElementById("quiz");
+const finalScoreDiv = document.getElementById("finalScore");
+const highScoresDiv = document.getElementById("highScores");
