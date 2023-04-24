d7f888023f0065282114f9448c13161aa4a18e81
Frank-Colorado <chawnkiee@gmail.com>
2023-04-23 21:46:10 -0400

    added 'startQuiz' function

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ const timerDisplay = document.getElementById("timer");
 const resultsDisplay = document.getElementById("results");
 
 // DOM Main Section Variables
-const responseDisplay = document.getElementById("responseTxt");
+const responseDisplay = document.getElementById("response");
 
 // DOM Home Page Section Variables
 const homePageDiv = document.getElementById("home");
@@ -12,6 +12,7 @@ const start = document.getElementById("startBtn");
 // DOM Quiz Section Variables
 const quizDiv = document.getElementById("quiz");
 const questionDisplay = document.getElementById("question");
+const allanswers = document.querySelectorAll(".answerBtn");
 const answer1 = document.getElementById("answerBtn1");
 const answer2 = document.getElementById("answerBtn2");
 const answer3 = document.getElementById("answerBtn3");
@@ -27,3 +28,10 @@ const initialsBtn = document.getElementById("initialBtn");
 const highScoresDiv = document.getElementById("highScores");
 const homeBtn = document.getElementById("homeBtn");
 const clearBtn = document.getElementById("clearBtn");
+
+// Start Quiz Function
+const startQuiz = () => {
+  homePageDiv.classList.add("d-none");
+  responseDisplay.innerText = "";
+  quizDiv.classList.remove("d-none");
+};
