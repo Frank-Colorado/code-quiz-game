28bb5f9b168379e5496eea263c35df7e2dbb39ee
Frank-Colorado <chawnkiee@gmail.com>
2023-04-23 21:30:14 -0400

    added variables for all DOM elements with comments

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,29 @@
 // DOM Navbar Variables
-const timer = document.getElementById("timer");
-// DOM DIVs
+const timerDisplay = document.getElementById("timer");
+const resultsDisplay = document.getElementById("results");
+
+// DOM Main Section Variables
+const responseDisplay = document.getElementById("responseTxt");
+
+// DOM Home Page Section Variables
 const homePageDiv = document.getElementById("home");
+const start = document.getElementById("startBtn");
+
+// DOM Quiz Section Variables
 const quizDiv = document.getElementById("quiz");
+const questionDisplay = document.getElementById("question");
+const answer1 = document.getElementById("answerBtn1");
+const answer2 = document.getElementById("answerBtn2");
+const answer3 = document.getElementById("answerBtn3");
+const answer4 = document.getElementById("answerBtn4");
+
+// DOM Final Score Section Variables
 const finalScoreDiv = document.getElementById("finalScore");
+const userScoreDisplay = document.getElementById("userScore");
+const initialsInput = document.getElementById("initials");
+const initialsBtn = document.getElementById("initialBtn");
+
+// DOM High Scores Section Variables
 const highScoresDiv = document.getElementById("highScores");
+const homeBtn = document.getElementById("homeBtn");
+const clearBtn = document.getElementById("clearBtn");
